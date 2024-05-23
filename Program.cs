e9dffbd86ea489fa780ada3dba20821cda749f1f
toandzpro <kimthoai15620@gmail.com>
2024-05-24 04:48:14 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿public class bai6
+{
+    static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        System.Console.WriteLine("Datetime Now is :" + localDate);
+    }
+}
+
+   
